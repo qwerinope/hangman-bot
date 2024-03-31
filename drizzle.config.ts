0fd03ca95bd292efa3d3d95a752959a324b57019
qweri0p <71005650+qweri0p@users.noreply.github.com>
2024-03-31 21:36:11 +0200

    removed reference to the bygone sqlite era

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,12 +8,3 @@ export default {
 		connectionString: 'postgres://postgres:123password@db:5432/postgres'
 	}
 } satisfies Config;
-
-/*export default {
-	driver: 'better-sqlite',
-	schema: './src/drizzle/schema.ts',
-	dbCredentials: {
-		url: './db/data.sqlite'
-	}
-
-} satisfies Config;*/
